1c02041ca2197ba56208139a18011327f5b85ed4
hwaniiidev <hwaniiidev@gmail.com>
2021-04-20 08:01:13 +0900

    3/옵저버블 기초
    - 옵저버블, 옵저버 개념 이해

--- rxkotlintraining/src/main/java/com/sample/rxkotlintraining/Observer.kt
+++ rxkotlintraining/src/main/java/com/sample/rxkotlintraining/Observer.kt
@@ -1,17 +1,48 @@
 package com.sample.rxkotlintraining
 
-import io.reactivex.Observable
-import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.rxkotlin.toObservable
-
 
 fun main() {
-    val list:List<Any> = listOf("One", 2, "Three", "Four", 4.5, "Five", 6.0f)
+    /* 테스트용
+    val list: List<Any> = listOf("One", 2, "Three", "Four", 4.5, "Five", 6.0f)
     val observable: Observable<Any> = list.toObservable()
 
     observable.subscribeBy(
-        onNext = { println(it) },
-        onError =  { it.printStackTrace() },
-        onComplete = { println("Done!") }
-    )
+            onNext = { println(it) },
+            onError = { it.printStackTrace() },
+            onComplete = { println("Done!") }
+    )*/
+
+    /* 옵저버블 기초
+    val observer: Observer<Any> = object : Observer<Any> {
+        override fun onComplete() {
+            // 구독 완료
+            println("onComplete")
+        }
+
+        override fun onSubscribe(d: Disposable) {
+            // 구독 시작
+            println("onSubscribe $d")
+        }
+
+        override fun onNext(t: Any) {
+            // 옵저버블이 내보내는 각 아이템 호출
+            println("onNext $t")
+        }
+
+        override fun onError(e: Throwable) {
+            // 오류 발생
+            println("onError $e")
+        }
+    }
+
+    val observable: Observable<Any> =
+            listOf("One", 2, "Three", "Four", 4.5, "Five", 6.0f).toObservable()
+
+    observable.subscribe(observer)  //todo safeSubscribe() ??, sorted()??, skip()??, sample()?? 등등
+
+    val observableOnList: Observable<List<Any>> =
+            Observable.just(listOf("One", 2, "Three", "Four", 4.5, "Five", 6.0f),
+            listOf("List with Single Item"),
+            listOf(1,2,3,4,5,6))
+    observableOnList.subscribe(observer)*/
 }
