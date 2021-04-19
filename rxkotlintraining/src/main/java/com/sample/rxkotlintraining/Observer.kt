bcd17d03fd7ac9ac0f134fa0f1ecce6dfeaec0c2
hwaniiidev <hwaniiidev@gmail.com>
2021-04-19 09:37:45 +0900

    feat/rxKotlin 추가
    - training용 lib폴더 추가

---
+++ rxkotlintraining/src/main/java/com/sample/rxkotlintraining/Observer.kt
@@ -0,0 +1,17 @@
+package com.sample.rxkotlintraining
+
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toObservable
+
+
+fun main() {
+    val list:List<Any> = listOf("One", 2, "Three", "Four", 4.5, "Five", 6.0f)
+    val observable: Observable<Any> = list.toObservable()
+
+    observable.subscribeBy(
+        onNext = { println(it) },
+        onError =  { it.printStackTrace() },
+        onComplete = { println("Done!") }
+    )
+}
