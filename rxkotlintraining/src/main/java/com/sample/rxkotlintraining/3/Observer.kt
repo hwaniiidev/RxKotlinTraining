81db6ca9d4799eab23fb043399bcf6ded5686fac
hwaniiidev <hwaniiidev@gmail.com>
2021-04-25 14:45:17 +0900

    feat/Hot observable 의 이해2

--- rxkotlintraining/src/main/java/com/sample/rxkotlintraining/3/Observer.kt
+++ rxkotlintraining/src/main/java/com/sample/rxkotlintraining/3/Observer.kt
@@ -38,7 +38,7 @@ fun main() {
 
     /*COLD / HOT observable
     COLD는 CD/DVD 레코딩으로 컨텐츠를 본다면 HOT 은 TV채널과 비슷 시청자가 시청하는지 여부에 관계없이 콘텐츠 계속 배출.
-     */
+     *//*
     val connectableObservable =
         listOf("String1","String2","String3","String4","String5").toObservable().publish()
     connectableObservable.subscribe({println("Subscription1: $it")})
@@ -46,5 +46,21 @@ fun main() {
     // connect 이전에 구독했던 observable 에게만 배출
     connectableObservable.connect()
     connectableObservable.subscribe({println("Subscription3: $it")})
+    connectableObservable.connect()*/
+
+    var connectableObservable =
+        Observable.interval(100, TimeUnit.MILLISECONDS).publish()
+    val disposable = connectableObservable.subscribe({ println("Subscription1: $it") })
+    connectableObservable.subscribe({ println("Subscription2: $it") })
     connectableObservable.connect()
+    Thread.sleep(500)
+    println("sleep")
+    disposable.dispose()
+
+    // subscribe로 반환되는 disposable를 dispose 하기 전까진 이미 connect된 observable은 해지 안됨?
+    connectableObservable =
+        listOf(11L, 22L, 33L, 44L, 55L).toObservable().publish()
+    connectableObservable.subscribe({ println("Subscription3: $it") })
+    connectableObservable.connect()
+    Thread.sleep(500)
 }
