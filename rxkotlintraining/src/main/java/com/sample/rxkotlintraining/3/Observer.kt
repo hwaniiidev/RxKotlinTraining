6152b302fd2b8610e69891e206d74a3e32fabb47
shinhwankim <sinhwan0211@loplat.com>
2021-04-25 14:04:02 +0900

    feat/Cold,Hot observable 의 이해

--- rxkotlintraining/src/main/java/com/sample/rxkotlintraining/3/Observer.kt
+++ rxkotlintraining/src/main/java/com/sample/rxkotlintraining/3/Observer.kt
@@ -3,11 +3,12 @@ package com.sample.rxkotlintraining.`3`
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.toObservable
 import java.util.concurrent.TimeUnit
 
 fun main() {
-    // observer를 구독 중에 중단 시키기
-    val observale: Observable<Long> = Observable.interval(100,TimeUnit.MILLISECONDS)
+    /*// observer를 구독 중에 중단 시키기
+    val observable: Observable<Long> = Observable.interval(100,TimeUnit.MILLISECONDS)
     val observer: Observer<Long> = object : Observer<Long> {
         lateinit var disposable: Disposable
 
@@ -32,6 +33,18 @@ fun main() {
         }
     }
 
-    observale.subscribe(observer)
-    Thread.sleep(1500)
+    observable.subscribe(observer)
+    Thread.sleep(1500)*/
+
+    /*COLD / HOT observable
+    COLD는 CD/DVD 레코딩으로 컨텐츠를 본다면 HOT 은 TV채널과 비슷 시청자가 시청하는지 여부에 관계없이 콘텐츠 계속 배출.
+     */
+    val connectableObservable =
+        listOf("String1","String2","String3","String4","String5").toObservable().publish()
+    connectableObservable.subscribe({println("Subscription1: $it")})
+    connectableObservable.map(String::reversed).subscribe({println("Subscription2: $it")})
+    // connect 이전에 구독했던 observable 에게만 배출
+    connectableObservable.connect()
+    connectableObservable.subscribe({println("Subscription3: $it")})
+    connectableObservable.connect()
 }
